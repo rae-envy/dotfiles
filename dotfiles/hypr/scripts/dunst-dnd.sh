99b50bdd7f4cbec41b95b6881c4ae195dfe79f58
rae-envy <officialrae.envy@gmail.com>
2024-08-11 11:55:56 -0500

    Add files via upload

---
+++ dotfiles/hypr/scripts/dunst-dnd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# function dunst_show_history() {
+#     hist_size=$(dunstctl history | jq '.data[0] | length')
+#     for i in `seq $hist_size`; do
+#         dunstctl history-pop
+#     done
+# }
+
+# # function dunst_dnd_stop() {
+# #     dunstctl rule dnd disable
+# #     dunst_show_history
+# # }
+
+# # function dunst_dnd_start() {
+# #     dunstctl history-clear
+# #     dunstctl rule dnd enable
+# # }
+
+
